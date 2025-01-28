de065cb6030566c01c6f2a55d58ec1444b78ea37
feel2code <feel2code@proton.me>
2025-01-28 13:37:24 +0400

    copilot support added

--- nvim/lua/custom/chadrc.lua
+++ nvim/lua/custom/chadrc.lua
@@ -2,7 +2,7 @@
 local M = {}
 
 M.ui = {
-  theme = 'catppuccin',
+  theme = 'chadracula',
   transparency = true,
   statusline = {
     theme = "vscode_colored",
@@ -13,5 +13,11 @@ M.ui = {
 }
 M.plugins = "custom.plugins"
 vim.opt.relativenumber = true
+
+vim.keymap.set('i', '<C-y>', 'copilot#Accept("\\<CR>")', {
+  expr = true,
+  replace_keycodes = false
+})
+vim.g.copilot_no_tab_map = true
 
 return M
